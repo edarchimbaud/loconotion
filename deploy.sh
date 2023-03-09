5a2841f3edd3a2c35d7bc027690ca48ba70d87b3
edarchimbaud <edouard.darchimbaud@gmail.com>
2023-03-09 22:13:08 +0100

    feat: added organisation of the folder + sitemap

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 rm -r dist/edarchimbaud
 python loconotion edarchimbaud/edarchimbaud.toml
-(cd ../edarchimbaud.github.io; rm *)
-cp dist/edarchimbaud/* ../edarchimbaud.github.io/.
+(cd dist/edarchimbaud; python ../../loconotion/modules/locorg.py)
+(cd ../edarchimbaud.github.io; rm -r *)
+cp -r dist/edarchimbaud/* ../edarchimbaud.github.io/.
 echo "# edarchimbaud.github.io" > ../edarchimbaud.github.io/README.md
 echo ".DS_Store" > ../edarchimbaud.github.io/.gitignore
 (cd ../edarchimbaud.github.io; git add *; git commit -m "feat: new version"; git pull; git push)
